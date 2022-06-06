aec4ad205d57b1105029e5693011f76c2264bd28
Bent Prellberg <pre@biba.uni-bremen.de>
2022-06-06 15:17:18 +0200

    lidar_mqtt.py

--- lidar_mqtt.py
+++ lidar_mqtt.py
@@ -7,6 +7,7 @@ def lidar_dic_test(id="urn:uni-bremen:bik:wio:0:0:wnds:0002", temperature = None
               airPressure = None, ground_windspeed = None, tilt = None, humidity = None, raining = None, met_wind_direction = None,pod_upper_temperature = None,
               pod_lower_temperature = None,pod_humidity = None,gps_latitude = None, gps_longitude = None,  timestamp_data_received = None, reference=None, timestamp_data_generated= None, scan_dwell_time= None):
     #creating specific .json structure to be able to use the data on a server
+    global client
     data =  {
     "content-spec": "urn:spec://eclipse.org/unide/measurement-message#v3",
     "device": {
@@ -156,76 +157,81 @@ def lidar_dic_test(id="urn:uni-bremen:bik:wio:0:0:wnds:0002", temperature = None
         pass
     return data_json
 
-with open('lidar-mqtt.json') as json_file:
-    config = json.load(json_file)
-    print(config)
-    user = config['user']
-    password = config['password']
-    url = config['url']
-    port = config['port']
-    edge_id = config['edge_id']
-    device_id = config['device_id']
-    mqtt_topic = "ppmpv3/3/DDATA/" + edge_id + "/" + device_id
+def mqtt_connection():
+    global client
+    with open('lidar-mqtt.json') as json_file:
+        config = json.load(json_file)
+        print(config)
+        user = config['user']
+        password = config['password']
+        url = config['url']
+        port = config['port']
+        edge_id = config['edge_id']
+        device_id = config['device_id']
+        mqtt_topic = "ppmpv3/3/DDATA/" + edge_id + "/" + device_id
 
-client = mqtt.Client()
-print("Working with user: " + user)
-client.username_pw_set(user, password)
-client.connect(url, port)
-print("Successfully connected.")
-client.tls_set_context(mqtt_ssl.create_default_context())
-client.loop_start()
+    client = mqtt.Client()
+    print("Working with user: " + user)
+    client.username_pw_set(user, password)
+    client.connect(url, port)
+    print("Successfully connected.")
+    client.tls_set_context(mqtt_ssl.create_default_context())
+    client.loop_start()
 
-data = lidar_dic_test()
-context = zmq.Context()
-socket = context.socket(zmq.SUB)
-try:
-    #connecting to the same ip-adress and port as the publisher
-    socket.connect('tcp://127.0.0.1:5557')
-except Exception as e :
-    logging.FATAL(f'Failed to bind to zeromq socket:{e}')
-    sys.exit(-1)
+    data = lidar_dic_test()
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    try:
+        #connecting to the same ip-adress and port as the publisher
+        socket.connect('tcp://127.0.0.1:5557')
+    except Exception as e :
+        logging.FATAL(f'Failed to bind to zeromq socket:{e}')
+        sys.exit(-1)
 
-socket.setsockopt_string(zmq.SUBSCRIBE, '')
-print('Successfully bound to zeroMQ receiver socket as subscriber')
-first_message = True
-print('Trying to receive data.')
-try:
-    while True:
-        message = socket.recv_pyobj() #receives data and ZMQ-topic
-        zmq_topic = message[0].decode('utf-8')
+    socket.setsockopt_string(zmq.SUBSCRIBE, '')
+    print('Successfully bound to zeroMQ receiver socket as subscriber')
+    first_message = True
+    print('Trying to receive data.')
+    try:
+        while True:
+            message = socket.recv_pyobj() #receives data and ZMQ-topic
+            zmq_topic = message[0].decode('utf-8')
 
-        if first_message:
-            print(f'Received first data: {message}')
-            print(f'From topic: {zmq_topic}')
-            first_message = False
+            if first_message:
+                print(f'Received first data: {message}')
+                print(f'From topic: {zmq_topic}')
+                first_message = False
 
-        if zmq_topic == "ldr": #filling the .json structure with the data published by zmqPUB.py
-            data = lidar_dic_test(
-                             temperature=message[1]["met_station"]["temperature"],
-                             battery=message[1]["met_station"]["battery"],
-                             airPressure=message[1]["met_station"]["airPressure"],
-                             ground_windspeed=message[1]["met_station"]["windspeed"],
-                             tilt=message[1]["met_station"]["tilt"],
-                             humidity=message[1]["met_station"]["humidity"],
-                             raining=message[1]["met_station"]["raining"],
-                             met_wind_direction=message[1]["met_station"]["met_wind_direction"],
-                             pod_upper_temperature=message[1]["met_station"]["pod_upper_temperature"],
-                             pod_lower_temperature=message[1]["met_station"]["pod_lower_temperature"],
-                             pod_humidity=message[1]["met_station"]["pod_humidity"],
-                             gps_latitude=message[1]["met_station"]["gps_latitude"],
-                             gps_longitude=message[1]["met_station"]["gps_longitude"],
-                             timestamp_data_received=message[1]["timestamp_data_received"],
-                             reference=message[1]["reference"],
-                             timestamp_data_generated=str(message[1]["timestamp_data_generated"]),
-                             scan_dwell_time=message[1]["met_station"]["scan_dwell_time"])
-            print(data)
-            client.publish(mqtt_topic,data)
-        else:
-            print(f'Only use topic "ldr" is used, however I received data on topic: {zmq_topic}')
-        sleep(1)
-        continue
+            if zmq_topic == "ldr": #filling the .json structure with the data published by zmqPUB.py
+                data = lidar_dic_test(
+                                 temperature=message[1]["met_station"]["temperature"],
+                                 battery=message[1]["met_station"]["battery"],
+                                 airPressure=message[1]["met_station"]["airPressure"],
+                                 ground_windspeed=message[1]["met_station"]["windspeed"],
+                                 tilt=message[1]["met_station"]["tilt"],
+                                 humidity=message[1]["met_station"]["humidity"],
+                                 raining=message[1]["met_station"]["raining"],
+                                 met_wind_direction=message[1]["met_station"]["met_wind_direction"],
+                                 pod_upper_temperature=message[1]["met_station"]["pod_upper_temperature"],
+                                 pod_lower_temperature=message[1]["met_station"]["pod_lower_temperature"],
+                                 pod_humidity=message[1]["met_station"]["pod_humidity"],
+                                 gps_latitude=message[1]["met_station"]["gps_latitude"],
+                                 gps_longitude=message[1]["met_station"]["gps_longitude"],
+                                 timestamp_data_received=message[1]["timestamp_data_received"],
+                                 reference=message[1]["reference"],
+                                 timestamp_data_generated=str(message[1]["timestamp_data_generated"]),
+                                 scan_dwell_time=message[1]["met_station"]["scan_dwell_time"])
+                #print(data)
+                client.publish(mqtt_topic,data)
+            else:
+                print(f'Only use topic "ldr" is used, however I received data on topic: {zmq_topic}')
+            sleep(1)
+            continue
 
-except KeyboardInterrupt:
-    client.loop_stop()
-    print('Interrupted!')
-    pass
+    except KeyboardInterrupt:
+        client.loop_stop()
+        print('Interrupted!')
+        pass
+
+
+
