125e8d834cbb5a4bd1f1bae52793c19876da9f5d
Bent Prellberg <pre@biba.uni-bremen.de>
2022-06-28 09:11:51 +0200

    lidar_data.py

--- src/lidar_data.py
+++ src/lidar_data.py
@@ -13,6 +13,16 @@ import calendar
 
 class Lidar():
     def __init__(self):
+        """
+
+        Using the Modbus capability of the ZX300 Lidar it is possible to extract data by polling data from the
+        registers in the dictionaries below. Every register below contains data which refreshes after every full
+        measurement. (the time depends on the set number of heights)
+        The __init__ function also creates all necessary lists and other variables for either the use of the following
+        methods or the Modbus connection between the ZX300 and the MotionSensor Box.
+
+        """
+
         self.client = ModbusClient(host="10.10.8.1", port=502, auto_open=True)  # building connection to Lidar-Unit
         self.zmq_connection = ('tcp://127.0.0.1:5556')
         self.context = zmq.Context()
@@ -63,6 +73,10 @@ class Lidar():
         self.counter = 0
 
     def building_connection(self):
+        """
+        Building connection to the ZX300 using Modbus TCP/IP.
+        """
+
         try:
             self.client.open() == True
             print("connection established")
@@ -71,6 +85,11 @@ class Lidar():
             sys.exit(0)
 
     def initiate_zmq_connection(self):
+        """
+        Initiating a zmq connection for intern communication. The data extracted from the lidar
+        is published and is received by lidar_mqtt.py.
+        """
+
         logging.debug(f'binding to {self.zmq_connection} for zeroMQ IPC')
         try:
             self.socket.bind(self.zmq_connection)
@@ -82,6 +101,9 @@ class Lidar():
         logging.debug(f'entering endless loop')
 
     def publishing_data(self):
+        """
+        All data is published using ZeroMQ.
+        """
         LIDAR_TOPIC = "ldr".encode('utf-8')  # setting the Topic for ZMQ
         try:
             lidar_data = self.socket.send_multipart([LIDAR_TOPIC,pickle.dumps(self.data)])
@@ -90,21 +112,30 @@ class Lidar():
             sys.exit(-1)
 
     def start_up(self):
+        """
+        Essential method to receive information about the number of heights and
+        the set heights before polling data in a loop.
+        """
         self.number_of_heights = int(self.get_lidar_data(8200))
         self.reference = self.get_lidar_data(0)
         j = 0
         while j <= self.number_of_heights:
             for key in self.horizontal_windspeed_dic:
                 try:
-                    #self.horizontal_windspeed_list.append(self.get_lidar_data(self.horizontal_windspeed_dic.get(key)))
                     self.heights_list.append(self.get_lidar_data(self.heights_dic.get(key)))
                     j += 1
                 except:
-                    #self.horizontal_windspeed_list.append("one value is missing")
                     self.heights_list.append(f"set height value is missing")
                     j += 1
 
     def run(self):
+        """
+        The run function is the main method for receiving and sending data. Everytime the reference of a dataset changes
+        all data in the important registers is received, computed and later sent using ZeroMQ. The reference changes
+        each time when the lidar has made a full measurement (meaning all heights and the corresponding values
+        are measured).
+        The data is temporarily stored in a list (self.data)
+        """
         self.first_data = True
         while self.client.open() == True:
             self.referenceCur = self.get_lidar_data(0)
@@ -121,7 +152,6 @@ class Lidar():
                     sleep(0.2)
             else:
                 self.first_data = True
-                #self.data.extend([datetime.fromtimestamp(epoch := time(), tz=timezone.utc),epoch])
                 self.data = [str(calendar.timegm(datetime.fromtimestamp(timestamp=time(), tz=timezone.utc).utctimetuple())), uptime.uptime()]
                 self.data.append(self.reference)
                 self.data.append(self.timestamp)
@@ -136,12 +166,19 @@ class Lidar():
                     self.polling_wind_directions()
                     self.counter += 1
                 self.data.extend(self.met_station_data)
-                #print(self.data)
+
+                for i in range(len(self.data)):
+                    if self.data[i] == 9999.0:
+                        self.data[i] = None
+
                 self.publishing_data()
                 self.data.clear()
                 self.reference = self.referenceCur
 
     def polling_vertical_windspeeds(self):
+        """
+        Appends the value for the vertical wind speeds at the given height to the list.
+        """
         try:
             if self.counter == 0:
                 self.data.append(self.vertical_windspeed_1)
@@ -152,6 +189,9 @@ class Lidar():
             print(f"unable to receive value for this height(vertical_windspeed):{e}")
 
     def polling_wind_directions(self):
+        """
+        Appends the value for the wind direction at the given height to the list.
+        """
         try:
             if self.counter == 0:
                 self.data.append(self.wind_direction_1)
@@ -162,6 +202,9 @@ class Lidar():
             print(f"unable to receive value for this height(wind direction):{e}")
 
     def polling_horinzontal_windspeeds(self):
+        """
+        Appends the value for the horizontal wind speeds at the given height to the list.
+        """
         try:
             if self.counter == 0:
                 self.data.append(self.horizontal_windspeed_1)
@@ -172,8 +215,12 @@ class Lidar():
                 print(f"unable to receive value for this height(horizontal_windspeed):{e}")
 
     def polling_reference(self):
+        """
+        Appending for each measurement made by the lidar an individual reference number to the list (self.data).
+        Each height gets a unique reference number.
+        """
         try:
-            if self.counter == 0 :
+            if self.counter == 0:
                 self.current_reference = self.reference
                 self.data.append(self.current_reference)
             else:
@@ -186,7 +233,15 @@ class Lidar():
 
 
     def output_met_station(self):
-        #this dictionary contains all important data measured by the met_station
+        """
+        All important data measured by the met station and data concerning the status of the lidar is polled.
+
+        Return
+        ------
+        list
+            containing all met station data and data concerning the status of the lidar
+            Shape: len = 14
+        """
         try:
             self.met_station_data = [round(self.get_lidar_data(self.met_station_dic.get("temperature")),2),
                               round(self.get_lidar_data(self.met_station_dic.get("battery")),2),
@@ -207,14 +262,21 @@ class Lidar():
             print(f"unable to receive all data provided by the met station:{e}")
 
     def get_lidar_time_stamp(self):
-        #since the timestamp is 32 bit and the mantissa is only the size of 23 bits, the information
-        #for the timestamp is split into two registers.
-        #these two registers are combined and processed subsequently
+        """
+        Since the timestamp is 32 bit and the mantissa is only the size of 23 bits, the information
+        for the timestamp is split into two registers.
+        These two registers are combined and processed subsequently
+
+         Return
+         ------
+         time object
+            timestamp object representing the time the data was measured
+            Shape: timestamp with year|month|day|hour|minute|second|timezone info
+            """
+
         self.timestamp_dic = {"TS_top": None ,"TS_bottom":None }
         self.timestampTop = self.client.read_input_registers(36,2)
         self.timestampBottom= self.client.read_input_registers(38,2)
-        #print(self.timestampTop)
-        #print(self.timestampBottom)
         for i in self.timestampTop:
             if i == None:
                 return False
@@ -227,7 +289,6 @@ class Lidar():
             else:
                 self.timestamp_dic["TS_bottom"] = self.hex_to_float(self.timestampBottom[0],self.timestampBottom[1])
         timestamp_add = self.timestamp_dic["TS_top"] + self.timestamp_dic["TS_bottom"]
-        #print(timestamp_add)
 
         year_stamp = timestamp_add / 60 / 60 / 24 / 31 / 12
         year_cal = str(year_stamp).split(".")
@@ -261,33 +322,83 @@ class Lidar():
             return self.timestamp
 
     def cal_date(self,factor, decimal):
+        """
+        Small calculation for date calculation
+
+        Parameters
+        ----------
+        factor: int
+                factor by which the remaining decimal needs to be multiplied with
+
+        decimal: list
+                 remaining decimal number of the previous calculation
+        Return
+        ------
+        list
+            returning the remaining value as a list
+            Shape: len = 2
+        """
         stamp = float("0." + str(decimal[1])) * factor
         cal = str(stamp).split(".")
         return cal
 
-    #since the lidar does not measure all horizontal windspeeds at the same time, individual
-    #timestamps are calculated, based on the scan_dwell_time
-    #the lidar outputs the highest placed height in the beginning and iterates trough the rest from heighest to lowest
+
     def individual_timestamp(self):
+        """
+        Since the lidar does not measure all horizontal wind speeds at the same time, individual
+        timestamps are calculated, based on the scan_dwell_time. The scan_dwell_time is add to the
+        timestamp of the first timestamp.
+        The lidar outputs the highest placed height in the beginning and iterates through the rest
+        from highest to lowest.
+        """
         timeobject = self.timestamp + timedelta(seconds=self.counter)
         self.data.append(calendar.timegm(timeobject.utctimetuple()))
 
 
     def get_lidar_data(self, attribute):
-        #'hex' is a list which contains two decimal numbers which represent the data contained in the specific register
-        #self.dec_to_float() converts the data into interpretable form
+        """
+        'hex' is a list which contains two decimal numbers which represent the data contained in the specific register.
+        self.dec_to_float() converts the data into interpretable form.
+
+        Parameter
+        --------
+        attribute: int
+                   register number
+        Return
+        ------
+        float
+            returning the converted value in the specific register number
+            Shape : 1-dimensional
+        """
+
         hex = self.client.read_input_registers(attribute,2)
         for i in hex:
             if i == None:
                 return False
         return self.hex_to_float(hex[0], hex[1])
 
-    # the method dec_to_float converts the registers (decimal_numbers) to hex/decimal numbers
-    # the two hex/decimal numbers are than combined. It is important to note, that
-    # all data is stored in big endian format.
-    # The combined hex/decimal number is later translated to binary code which is than interpreted as a single
-    # precision float number (32bits)
+
     def hex_to_float(self, hex0, hex1):
+        """
+        The method dec_to_float converts the registers (decimal_numbers) to hex/decimal numbers.
+        The two hex/decimal numbers are than combined. It is important to note, that
+        all data is stored in big endian format.
+        The combined hex/decimal number is later translated to binary code which is than interpreted as a single
+        precision float number (32bits).
+
+        Parameter
+        ---------
+        hex0: float
+              value contained in the first register
+        hex1: float
+              value contained in the second register
+
+        Return
+        ------
+        float
+            calculated value contained in the two neighbouring registers
+            Shape: 1-dimensional
+        """
         hexA_long = hex(hex0)
         hexB_long = hex(hex1)
         try:
