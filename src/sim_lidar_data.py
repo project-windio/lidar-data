6b8d6f790c25145142586ca35e908bc3fa674301
Bent Prellberg <pre@biba.uni-bremen.de>
2022-07-06 18:53:02 +0200

    sim_lidar_data.py

--- src/sim_lidar_data.py
+++ src/sim_lidar_data.py
@@ -9,6 +9,7 @@ import sys
 import uptime
 import pickle
 import calendar
+from random import uniform
 
 class Lidar():
     def __init__(self):
@@ -61,7 +62,7 @@ class Lidar():
         logging.debug(f'entering endless loop')
         while True:
             self.data = self.gen_lidar_message()
-            #print(self.data)
+            print(self.data)
             self.publish_data()
             self.data.clear()
             sleep(2) # Wait for 2 seconds.
@@ -79,11 +80,11 @@ class Lidar():
         
         # ToDo: Compute-generate hard-coded time stamp, vary measurement values by using random numbers (e.g. value + random_mumber or value * random_number)
         data =  [str(calendar.timegm(datetime.fromtimestamp(timestamp=time(), tz=timezone.utc).utctimetuple())), uptime.uptime(), 2027075.0, '2022-06-15 08:49:22+00:00',
-                 200.0, 2027075.0, 1655282962, 3.4464, 0.2488, 190.9362, 180.0, 2027076.0,
-                 1655282963, 4.0556, -0.0248, 183.9466, 160.0, 2027077.0, 1655282964, 3.9232, -0.0083, 185.4916, 140.0, 2027078.0, 1655282965, 3.8143, 0.038,
-                 183.4078, 120.0, 2027079.0, 1655282966, 3.4729, 0.0513, 184.1194, 100.0, 2027080.0, 1655282967, 2.8372, -0.0333, 180.4605, 80.0, 2027081.0,
-                 1655282968, 2.6685, -0.1191, 177.5728, 60.0, 2027082.0, 1655282969, 2.7555, -0.2547, 187.6137, 40.0, 2027083.0, 1655282970, 9999.0, 9999.0,
-                 9999.0, 20.0, 2027084.0, 1655282971, 3.3366, -0.1468, 160.2561, 0.0, 2027085.0, 1655282972, 9999.0, 9999.0, 9999.0, 22.6, 12.28, 1021.6, 0.31,
+                 200.0, 2027075.0, 1655282962, round(uniform(0,5),4), round(uniform(-2,2),4), round(uniform(180,200),4), 180.0, 2027076.0,
+                 1655282963, round(uniform(0,5),4),round(uniform(-2,2),4), round(uniform(180,200),4), 160.0, 2027077.0, 1655282964, round(uniform(0,5),4), round(uniform(-2,2),4),round(uniform(180,200),4), 140.0, 2027078.0, 1655282965, round(uniform(0,5),4),round(uniform(-2,2),4),
+                 round(uniform(180,200),4), 120.0, 2027079.0, 1655282966, round(uniform(0,5),4), round(uniform(-2,2),4), round(uniform(180,200),4), 100.0, 2027080.0, 1655282967, round(uniform(0,5),4), round(uniform(-2,2),4), round(uniform(180,200),4), 80.0, 2027081.0,
+                 1655282968, round(uniform(0,5),4), round(uniform(-2,2),4),round(uniform(180,200),4), 60.0, 2027082.0, 1655282969, round(uniform(0,5),4), round(uniform(-2,2),4),round(uniform(180,200),4), 40.0, 2027083.0, 1655282970, round(uniform(0,5),4), round(uniform(-2,2),4),
+                 round(uniform(180,200),4), 20.0, 2027084.0, 1655282971, round(uniform(0,5),4), round(uniform(-2,2),4),round(uniform(180,200),4), 0.0, 2027085.0, 1655282972, round(uniform(0,5),4), round(uniform(-2,2),4),round(uniform(180,200),4), 22.6, 12.28, 1021.6, 0.31,
                  0.0, 41.9, 0.0, 213.2, 38.0, 38.0, 23.0, 53.11, 8.86, 1.0]
         return data
 
